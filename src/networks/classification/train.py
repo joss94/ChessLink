aff24a57a5018dd30a32167e623952986fec071a
Josselin Manceau <josselin.manceau@7sensingsoftware.com>
2024-03-15 16:04:34 +0100

    Clean training

--- src/networks/classification/train.py
+++ src/networks/classification/train.py
@@ -1,17 +1,38 @@
 from ultralytics import YOLO
 
+TRAIN = True
+RESUME = False
+
+NAME = "test"
+DEVICE = 0
+DATASET = "/workspace/ChessLink/data/dataset_pieces/"
+
 # Load a model
-model = YOLO("yolov8m-cls.pt")  # load a pretrained model (recommended for training)
-# model = YOLO('/workspace/CL/runs/train/last.pt')  # load a pretrained model (recommended for training)
+if TRAIN and not RESUME:
+    model = YOLO("yolov8m-cls.pt")  # load a pretrained model (recommended for training)
+else:
+    model = YOLO(
+        f"/workspace/ChessLink/runs/detect/{NAME}/weights/last.pt"
+    )
 
 # Train the model with 2 GPUs
-results = model.train(
-    data="/workspace/ChessLink/data/dataset_pieces/",
-    epochs=1000,
-    imgsz=128,
-    device=0,
-    patience=1000,
-    augment=True,
-    batch=8000,
-    # fraction=0.1
+if TRAIN:
+    results = model.train(
+        name=NAME,
+        resume=RESUME,
+        data=DATASET,
+        device=DEVICE,
+
+        batch=8000,
+        patience=1000,
+        epochs=1000,
+        imgsz=128,
+        # fraction=0.1
 )
+
+metrics = model.val()  # no arguments needed, dataset and settings remembered
+
+metrics.top1
+metrics.top5
+
+print(metrics)
